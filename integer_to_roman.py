4c9bbd855edb5ed7154ffec0e5c5c4101686381a
wallermatt <waller.matthew.alt@gmail.com>
2022-08-03 14:54:48 +0100

    roman

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -13,7 +13,8 @@ For example, 2 is written as II in Roman numeral, just two one's added together.
 which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for 
-four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
@@ -43,4 +44,34 @@ Constraints:
 
 1 <= num <= 3999
 '''
+class Solution:
+
+    roman_numerals = [
+        ('M', 1000),
+        ('CM', 900),
+        ('D', 500),
+        ('CD', 400),
+        ('C', 100),
+        ('XC', 90),
+        ('L', 50),
+        ('XL', 40),
+        ('X', 10),
+        ('IX', 9),
+        ('V', 5),
+        ('IV', 4),
+        ('I', 1),
+    ]
+    
+    def intToRoman(self, num: int) -> str:
+        output = ''
+        for symbol, value in self.roman_numerals:
+            while num - value >= 0:
+                num -= value
+                output += symbol
+        return output
+
+s = Solution()
+assert s.intToRoman(3) == "III"
+assert s.intToRoman(1994) == "MCMXCIV"
+
 
