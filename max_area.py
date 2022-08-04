dc47428cc0023db6325c0e01b4fb47f841add83f
Matthew Waller <waller.matthew.alt@gmail.com>
2022-08-04 21:10:46 +0100

    atoi

--- max_area.py
+++ max_area.py
@@ -48,17 +48,33 @@ class Solution:
         return max_area
 
     def maxArea2(self, array):
-        left_max = [0,0]
-        right_max = [0,0]
         len_ = len(array)
+        if len_ == 2:
+            return min(array[0], array[1])
+
+        max_at_pos = [0 for e in range(len_)]
+        for i,e in array:
+            if i == 0:
+                max_at_pos[0] = e
+            elif e > max_at_pos[i-1]:
+                max_at_pos[i] = e
+            else:
+                max_at_pos[i] = max_at_pos[i-1]
+        
+
+        left_max = [0,0]
+        right_max = [0,len_]
         for i, e in enumerate(array):
-            left = e * (len_ - i)
+            if i == 0
+            left = min(max_at_pos[i-1], e) * (len_ - i)
             right = e * (i + 1)
-            if left > left_max[0]:
+            if len_ - i == i + 1:
+                continue
+            if left > left_max[0] and i < right_max[1]:
                 left_max = [left, i]
-            if right > right_max[0]:
+            if right > right_max[0] and i > left_max[1]:
                 right_max = [right, i]
-        #print(left_max, right_max, min(array[right_max[1]], array[left_max[1]]) * right_max[1] - left_max[1])
+        print(left_max, right_max, min(array[right_max[1]], array[left_max[1]]) * right_max[1] - left_max[1])
         return min(array[right_max[1]], array[left_max[1]]) * (right_max[1] - left_max[1])
             
 
@@ -67,6 +83,11 @@ class Solution:
 
 
 s = Solution()
+
+assert s.maxArea2([1,2,4,3]) == 4
+assert s.maxArea2([2, 1]) == 1
+assert s.maxArea2([1,2, 1]) == 2
+
 assert s.maxArea([1,1]) == 1
 assert s.maxArea([1,8,6,2,5,4,8,3,7]) == 49
 
