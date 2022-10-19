4a46533ea7e5fab7b7da6d959e4248293e702d7f
Matthew Waller <waller.matthew.alt@gmail.com>
2022-10-19 20:04:16 +0100

    knapsack

---
+++ knapsack.py
@@ -0,0 +1,13 @@
+'''
+EXERCISE
+9.2 Suppose you’re going camping. You have a knapsack that will hold
+6 lb, and you can take the following items. Each has a value, and the
+higher the value, the more important the item is:
+• Water, 3 lb, 10
+• Book, 1 lb, 3
+• Food, 2 lb, 9
+• Jacket, 2 lb, 5
+• Camera, 1 lb, 6
+What’s the optimal set of items to take on your camping trip?
+'''
+def knapsack()
