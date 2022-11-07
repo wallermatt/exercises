ed90ec9ace74aedcd20d4e2215dda3e754ca170b
Matthew Waller <waller.matthew.alt@gmail.com>
2022-11-07 13:44:51 +0000

    zombies + sprites

---
+++ zombies.py
@@ -0,0 +1,20 @@
+'''
+1. draw arena
+2. draw squares
+3. move squares
+'''
+import pygame
+
+
+CHAR_SIZE = 8
+ARENA_WIDTH = 20
+ARENA_HIGHT = 20
+
+
+
+
+pygame.init()
+
+display_screen = pygame.display.set_mode((800, 800))
+
+
