06b427d45e6fa706d548297992b874bb89730ab7
Matthew Waller <waller.matthew.alt@gmail.com>
2022-11-07 17:52:27 +0000

    pygane

--- zombies.py
+++ zombies.py
@@ -4,17 +4,86 @@
 3. move squares
 '''
 import pygame
+import random
 
 
-CHAR_SIZE = 8
+CHAR_SIZE = 16
 ARENA_WIDTH = 20
-ARENA_HIGHT = 20
+ARENA_HEIGHT = 20
+
+GREEN = (20, 255, 140)
+GREY = (210, 210 ,210)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
 
 
+class NPC(pygame.sprite.Sprite):
+    def __init__(self, type, column, row):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([CHAR_SIZE, CHAR_SIZE])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+ 
+        # Draw the car (a rectangle!)
+        #pygame.draw.rect(self.image, type, [column * CHAR_SIZE, row * CHAR_SIZE, CHAR_SIZE, CHAR_SIZE])
+ 
+        self.rect = pygame.draw.rect(self.image, type, [0, 0, CHAR_SIZE, CHAR_SIZE])
+
+        self.rect.x = 100 + column * CHAR_SIZE
+        self.rect.y = 100 + row * CHAR_SIZE
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        #self.rect = self.image.get_rect()
+ 
+    def moveRight(self, pixels):
+        if not self.rect.x + pixels >= 100 + ARENA_WIDTH * CHAR_SIZE:
+            self.rect.x += pixels
+ 
+    def moveLeft(self, pixels):
+        if not self.rect.x + pixels <= 0:
+            self.rect.x -= pixels
 
 
 pygame.init()
 
-display_screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Zombies")
+
+carryOn = True
+clock=pygame.time.Clock()
+
+all_sprites_list = pygame.sprite.Group()
 
 
+for _ in range(10):
+    all_sprites_list.add(NPC(RED, random.randrange(0, ARENA_WIDTH), random.randrange(0, ARENA_HEIGHT)))
+
+
+while carryOn:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                carryOn=False
+            elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_x: #Pressing the x Key will quit the game
+                     carryOn=False
+
+        for s in all_sprites_list:
+            s.moveRight(CHAR_SIZE)
+        all_sprites_list.update()
+
+        pygame.draw.rect(screen, WHITE, [100,100, ARENA_WIDTH * CHAR_SIZE, ARENA_HEIGHT * CHAR_SIZE])
+        
+        all_sprites_list.draw(screen)
+
+        #Refresh Screen
+        pygame.display.flip()
+ 
+        #Number of frames per second e.g. 60
+        clock.tick(1)
+ 
+pygame.quit() 
